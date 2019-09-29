ad21a15a8d294d8e5ef1b848a74696ad0af7f430
Lucas Coutinho <lucaspp1coutinho@gmail.com>
2019-09-29 18:51:35 -0300

    usuarioDao complete

---
+++ L2A/DAO/UsuarioDao.cs
@@ -0,0 +1,27 @@
+﻿using L2A.DAO.Service;
+using L2A.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2A.DAO
+{
+    class UsuarioDao
+    {
+        FileHelper<Usuario> fileHelper = new FileHelper<Usuario>();
+        public bool insert(Usuario usuario)
+        {
+            return fileHelper.Insert(usuario, out _);
+        }
+        public bool Update(Usuario usuario, int id)
+        {
+            return fileHelper.Update(usuario, id);
+        }
+        public bool Delete(int id)
+        {
+            return fileHelper.Delete(id);
+        }
+    }
+}
